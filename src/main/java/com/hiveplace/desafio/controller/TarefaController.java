cc50c2efc566854d5322822c509ad449d2ea8b84
Felipe Barros <felipeb4c@gmail.com>
2024-07-05 17:20:06 -0300

    Criando endpoint GET listando todas as tarefas

--- src/main/java/com/hiveplace/desafio/controller/TarefaController.java
+++ src/main/java/com/hiveplace/desafio/controller/TarefaController.java
@@ -3,13 +3,18 @@ package com.hiveplace.desafio.controller;
 import com.hiveplace.desafio.converter.TarefaConverter;
 import com.hiveplace.desafio.domain.Tarefa;
 import com.hiveplace.desafio.dto.CreateTarefaDTO;
+import com.hiveplace.desafio.dto.DetailTarefaDTO;
+import com.hiveplace.desafio.enums.StatusTarefa;
 import com.hiveplace.desafio.repository.TarefaRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @RequestMapping("/tarefa")
 @RestController
@@ -30,6 +35,12 @@ public class TarefaController {
         return Mono.just(model)
                 .flatMap(repository::save)
                 .doOnNext(it -> LOGGER.info("Tarefa salva com o id {}", it.getId()));
+    }
+
+    @GetMapping("/listarTodas")
+    public Flux<DetailTarefaDTO> listarTodasTarefas(){
+        Flux<Tarefa> listaTarefa = repository.findAll();
+        return repository.findAll().map(converter::toDTO);
     }
 
 
